0ca6c7052c0d474204b3f75bfb6416fbacd7e944
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-03-16 20:27:57 +0200

    add license

--- contracts/RewardsDistributionRecipient.sol
+++ contracts/RewardsDistributionRecipient.sol
@@ -1,17 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 // https://docs.synthetix.io/contracts/source/contracts/rewardsdistributionrecipient
 abstract contract RewardsDistributionRecipient {
     address public rewardsDistribution;
 
-    function notifyRewardAmount(uint256 reward) virtual external;
+    function notifyRewardAmount(uint256 reward) external virtual;
 
     modifier onlyRewardsDistribution() {
-        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
+        require(
+            msg.sender == rewardsDistribution,
+            "Caller is not RewardsDistribution contract"
+        );
         _;
     }
-// TODO?
-   /*  function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
-        rewardsDistribution = _rewardsDistribution;
-    } */
 }
